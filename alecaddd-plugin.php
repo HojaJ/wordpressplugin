0ba1e3d0cb75c06eea8d608f75e9548d670e9d18
Hodga <hojajepbar@gmail.com>
2019-01-29 23:31:26 +0200

    Lesson12

--- alecaddd-plugin.php
+++ alecaddd-plugin.php
@@ -32,10 +32,6 @@ if ( file_exists( dirname( __FILE__ ) . '/vendor/autoload.php' ) ) {
     require_once dirname( __FILE__ ) . '/vendor/autoload.php';
 }
 
-define('PLUGIN_PATH', plugin_dir_path(__FILE__));
-define('PLUGIN_URL', plugin_dir_url(__FILE__));
-define('PLUGIN', plugin_basename(__FILE__));
-
 use Inc\Base\Activate;
 use Inc\Base\Deactivate;
 
@@ -45,7 +41,9 @@ use Inc\Base\Deactivate;
 function activate_alecaddd_plugin() {
     Activate::activate();
 }
+
 register_activation_hook( __FILE__, 'activate_alecaddd_plugin' );
+
 
 /**
  * The code that runs during plugin deactivation
@@ -57,6 +55,9 @@ function deactivate_alecaddd_plugin() {
 register_deactivation_hook( __FILE__, 'deactivate_alecaddd_plugin' );
 
 
+/**
+ * Initialize all the core classes of the plugin
+ */
 if (class_exists('Inc\\Init')){
     Inc\Init::register_services();
 }
--- inc/Base/SettingsLinks.php
+++ inc/Base/SettingsLinks.php
@@ -6,15 +6,8 @@
 namespace Inc\Base;
 
 
-class SettingsLinks
+class SettingsLinks extends BaseController
 {
-    protected $plugin;
-
-    public function __construct()
-    {
-        $this->plugin = PLUGIN;
-    }
-
     public function register()
     {
         add_filter("plugin_action_links_$this->plugin", array($this, 'settings_link'));
