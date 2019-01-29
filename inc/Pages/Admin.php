278853bb01829febfce39b7018ab1fa563d0fe81
Hodga <hojajepbar@gmail.com>
2019-01-29 23:47:29 +0200

    Lesson15

--- inc/Pages/Admin.php
+++ inc/Pages/Admin.php
@@ -6,22 +6,61 @@
 namespace Inc\Pages;
 
 
+use Inc\Api\SettingsApi;
 use Inc\Base\BaseController;
 
 class Admin extends BaseController
 {
-    public function register() {
-        add_action( 'admin_menu', array( $this, 'add_admin_pages' ) );
+    public $settings;
+    public $pages = array();
+    public $subpages = array();
+
+    public function __construct()
+    {
+        $this->settings = new SettingsApi();
+
+        $this->pages = array(
+            array(
+                'page_title' => 'Alecaddd Plugin',
+                'menu_title' => 'Alecaddd',
+                'capability' => 'manage_options',
+                'menu_slug' => 'alecaddd_plugin',
+                'callback' => function() { echo '<h1>Alecaddd Plugin</h1>'; },
+                'icon_url' => 'dashicons-store',
+                'position' => 110
+            )
+        );
+
+        $this->subpages = array(
+            array(
+                'parent_slug' => 'alecaddd_plugin',
+                'page_title' => 'Custom Post Types',
+                'menu_title' => 'CPT',
+                'capability' => 'manage_options',
+                'menu_slug' => 'alecaddd_cpt',
+                'callback' => function() { echo '<h1>CPT Manager</h1>'; }
+            ),
+            array(
+                'parent_slug' => 'alecaddd_plugin',
+                'page_title' => 'Custom Taxonomies',
+                'menu_title' => 'Taxonomies',
+                'capability' => 'manage_options',
+                'menu_slug' => 'alecaddd_taxonomies',
+                'callback' => function() { echo '<h1>Taxonomies Manager</h1>'; }
+            ),
+            array(
+                'parent_slug' => 'alecaddd_plugin',
+                'page_title' => 'Custom Widgets',
+                'menu_title' => 'Widgets',
+                'capability' => 'manage_options',
+                'menu_slug' => 'alecaddd_widgets',
+                'callback' => function() { echo '<h1>Widgets Manager</h1>'; }
+            )
+        );
     }
 
-
-    public function add_admin_pages() {
-        add_menu_page( 'Alecaddd Plugin', 'Alecaddd', 'manage_options', 'alecaddd_plugin', array( $this, 'admin_index' ), 'dashicons-store', 110 );
+    public function register()
+    {
+        $this->settings->addPages($this->pages)->withSubPage('Dashboard')->addSubPages($this->subpages)->register();
     }
-
-
-    public function admin_index() {
-        require_once $this->plugin_path . 'templates/admin.php';
-    }
-
 }
